578613b0709aebc8a9f63cd65dd0d658f6937e44
SB2020-eye <56878280+SB2020-eye@users.noreply.github.com>
2022-08-22 22:47:49 -0400

    Update generated_config.py
    Changed paths
    (2nd change is a guess; not sure will work b/c folder doesn't exist; but I don't understand what all this is doing)

--- HTR_ctc/generated_data_loader/generated_config.py
+++ HTR_ctc/generated_data_loader/generated_config.py
@@ -1,6 +1,6 @@
 import os
 
-data_path = r'/HOME/pondenka/manuel/CycleGANRD/HTR_ctc/data/generated/'
+data_path = r'/content/drive/MyDrive/Github/Generating-Synthetic-Handwritten-Historical-Documents/HTR_ctc/data/generated/'
 data_image_names = os.listdir(data_path + '/EG-BG-LC    ')
 
-dataset_path = '/HOME/pondenka/manuel/CycleGANRD/HTR_ctc/saved_datasets/no/'
+dataset_path = '/content/drive/MyDrive/Github/Generating-Synthetic-Handwritten-Historical-Documents/HTR_ctc/saved_datasets/no/'
