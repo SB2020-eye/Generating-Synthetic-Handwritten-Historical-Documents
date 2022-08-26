aabe0f78616ad8354b0ea5723aaa21c8180581a8
SB2020-eye <56878280+SB2020-eye@users.noreply.github.com>
2022-08-25 20:47:24 -0400

    Update iam_config.py
    Changed paths

--- HTR_ctc/iam_data_loader/iam_config.py
+++ HTR_ctc/iam_data_loader/iam_config.py
@@ -3,10 +3,10 @@
 trainset_file = '/media/ncsr/bee4cbda-e313-4acf-9bc8-817a69ad98ae/IAM/set_split/trainset.txt'
 testset_file = '/media/ncsr/bee4cbda-e313-4acf-9bc8-817a69ad98ae/IAM/set_split/testset.txt'
 
-line_file = '/home/manuel/CycleGANRD/HTR_ctc/data/ascii/lines.txt'
-word_file = '/home/manuel/CycleGANRD/HTR_ctc/data/ascii/words.txt'
+line_file = '/content/drive/MyDrive/Github/Generating-Synthetic-Handwritten-Historical-Documents/HTR_ctc/data/ascii/lines.txt'
+word_file = '/content/drive/MyDrive/Github/Generating-Synthetic-Handwritten-Historical-Documents/HTR_ctc/data/ascii/words.txt'
 
-word_path = '/home/manuel/CycleGANRD/HTR_ctc/data/words'
-line_path = '/home/manuel/CycleGANRD/HTR_ctc/data/lines'
+word_path = '/content/drive/MyDrive/Github/Generating-Synthetic-Handwritten-Historical-Documents/HTR_ctc/data/words'
+line_path = '/content/drive/MyDrive/Github/Generating-Synthetic-Handwritten-Historical-Documents/HTR_ctc/data/lines'
 
-dataset_path = '/home/manuel/CycleGANRD/HTR_ctc/saved_datasets'
+dataset_path = '/content/drive/MyDrive/Github/Generating-Synthetic-Handwritten-Historical-Documents/HTR_ctc/saved_datasets'
